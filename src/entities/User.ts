ae43546ff7fcd51e85bf37a01ec27387bfadaf18
callum <cjoseph@aiudo.es>
2022-10-17 12:02:17 +0200

    written a detailed description of the relations between tables

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -25,6 +25,11 @@ export class User extends BaseEntity{
     
     @UpdateDateColumn()
     updatedAt: Date;
+
+    // The relation is made as follows: 'Post' = the class post in the post entities section. 'post' is an instance of 'Post', stored in a variable, 
+    // which contains all of the content of 'Post'. Then, in a callback function, the link is made to the other table with 'post.user'. 'user' will be definied 
+    // in the other table. Finally, 'posts: Post[];' is saying that the value 'posts' defined in the other table has the value of 'Post[]' which corresponds to the
+    // class Post
     
     @OneToMany(() => Post, post => post.user)
     posts: Post[];
